471f9edc284c3cdaab44b4c034087f55ae7a6d52
Dan Dailey <dan@officetoweb.com>
2020-12-14 10:41:37 -0800

    day 6

---
+++ 2020/06/06b.rb
@@ -0,0 +1,58 @@
+class Advent
+  def self.input; IO.read('input.txt'); end
+
+  def self.sample_input
+    <<-HEREDOC
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+HEREDOC
+  end
+
+  def self.print_answer
+    sum_of_unanimous_answer_counts_per_group = 0
+    input.split("\n\n").each do |raw_group_data|
+      answer_set_group = AnswerSetGroup.new(raw_group_data)
+      puts 'BEGIN ANSWER SET'
+      puts answer_set_group.raw_group_data
+      # puts '>> ' + answer_set_group.answer_set_list.inspect + "\n"
+      # puts '>> ' + answer_set_group.answer_set_list.inject{ |sum, n| sum + n }.inspect + "\n"
+      puts ">> #{answer_set_group.unanimous_answers.count} UNANIMOUS ANSWERS: " \
+        + answer_set_group.unanimous_answers.inspect + "\n\n"
+      sum_of_unanimous_answer_counts_per_group += answer_set_group.unanimous_answers.count
+    end
+    puts "ADVENT ANSWER: #{sum_of_unanimous_answer_counts_per_group}"
+  end
+end
+
+class AnswerSetGroup
+  attr_accessor :raw_group_data
+
+  def initialize(raw_group_data)
+    @raw_group_data = raw_group_data
+  end
+
+  def answer_set_list
+    @answer_set_list ||= raw_group_data.split("\n")
+  end
+
+  def unanimous_answers
+    @unanimous_answers ||= answer_set_list.
+      map{ |a| a.split('') }.
+      inject{ |sum, n| sum & n }
+  end
+end
+
+Advent.print_answer
